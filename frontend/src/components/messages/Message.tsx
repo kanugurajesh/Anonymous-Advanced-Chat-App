2a98274eac6207d0cf495b4e5cf2f6fbe41b64f7
kanugurajesh <kanugurajesh3@gmail.com>
2024-06-21 21:55:00 +0530

    Adding chat bubbles

--- frontend/src/components/messages/Message.tsx
+++ frontend/src/components/messages/Message.tsx
@@ -1,12 +1,23 @@
-const Message = () => {
+import { useAuthContext } from "../../context/AuthContext"
+import useConversation from "../../zustand/useConversation";
+
+const Message = ({ message }:any) => {
+  
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500": "bg-gray-600"
+
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
         <div className="chat-image avatar">
             <div className="w-10 rounded-full">
-                <img src="https://ucarecdn.com/a0e8647c-d1ee-4a00-a3d0-e9ea9462b7ea/examplelukeoslizlo.jpg" alt="Tailwind Css" />
+                <img alt="bubble component" src={profilePic} />
             </div>
         </div>
-        <div className={`chat-bubble text-white bg-blue-500`}>Hi | what is upp?</div>
+        <div className={`chat-bubble text-white bg-blue-500`}>{message.message}</div>
         <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12:42</div>
     </div>
   )
--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,24 +1,23 @@
 import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
-    const { messages, loading } = useGetMessages();
-    console.log(messages);
-    return (
-        <div className="px-4 flex-1 overflow-auto">
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-        </div>
-    )
-}
+  const { messages, loading } = useGetMessages();
+  console.log(messages);
+  // const { messages } = useGetMessages();
+  // const loading = true;
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+        {!loading && messages.length > 0 && messages.map((message:any) => (
+            <Message key={message._id} message={message} />
+        ))}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
+    </div>
+  );
+};
 
 export default Messages;
